bba987d26fc53b0df0f9e1414f45f14af991aba4
Keen Wilson <40321520+keenwilson@users.noreply.github.com>
2018-09-23 14:09:04 -0500

    Initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDJIB4hB5B7Ut5AP4QL8KV4F4FtU08BHXI",
+    authDomain: "rps-multiplayer-b9d2f.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-b9d2f.firebaseio.com",
+    projectId: "rps-multiplayer-b9d2f",
+    storageBucket: "rps-multiplayer-b9d2f.appspot.com",
+    messagingSenderId: "744450581138"
+};
+firebase.initializeApp(config);
