a1ef2b09e4d8f213c188c332b9200f1b1f144bd9
Keen Wilson <40321520+keenwilson@users.noreply.github.com>
2018-09-23 17:51:13 -0500

    Add notification, 3-column game area, and chat box in HTML

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,6 @@
 // Initialize Firebase
+// =============================================================
+
 var config = {
     apiKey: "AIzaSyDJIB4hB5B7Ut5AP4QL8KV4F4FtU08BHXI",
     authDomain: "rps-multiplayer-b9d2f.firebaseapp.com",
@@ -8,3 +10,50 @@ var config = {
     messagingSenderId: "744450581138"
 };
 firebase.initializeApp(config);
+
+var turn = 0;
+
+var playerOneName = "";
+var playerOneWins = "";
+var playerOneLosses = "";
+var playerOneChoice = "";
+
+var playerTwoName = "";
+var playerTwoWins = "";
+var playerTwoLosses = "";
+var playerTwoChoice = "";
+
+var chat = "";
+
+
+// Function to handler clicks
+// =============================================================
+
+// Click events for Start button
+$("#start-game").on("click", function () {
+    console.log("You've clicked a 'start'button.")
+    var name = $("#player-name").val().trim();
+    console.log(name);
+});
+
+// Click events for Rock, Paper, Scissors
+$("#player-1-rock").on("click", function () {
+    console.log("Player 1 clicked Rock.");
+});
+$("#player-1-paper").on("click", function () {
+    console.log("Player 1 clicked Paper.");
+});
+$("#player-1-scissors").on("click", function () {
+    console.log("Player 1 clicked Scissors.");
+});
+$("#player-2-rock").on("click", function () {
+    console.log("Player 2 clicked Rock.");
+});
+$("#player-2-paper").on("click", function () {
+    console.log("Player 2 clicked Paper.");
+});
+$("#player-2-scissors").on("click", function () {
+    console.log("Player 2 clicked Scissors.");
+});
+
+
